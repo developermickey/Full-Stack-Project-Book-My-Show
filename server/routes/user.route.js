2af7d59f56a6032b95341c47f4a423cf0ae01fde
developermickey <mukesh.quazma@gmail.com>
2025-10-29 22:53:35 +0530

    code refector using controller userController

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,36 +1,8 @@
 const express = require("express");
-const User = require("../models/user.model");
 const router = express.Router();
+const userController = require("../controllers/user.controller");
 // signup, login, get user, forgot password
 
-router.post("/register", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const userExist = await User.findOne({ email });
-    if (userExist) {
-      return res.status(401).json({
-        message: "User already exists please login",
-        success: false,
-      });
-    }
-
-    const user = await User.create({
-      name,
-      email,
-      password,
-    });
-
-    return res.status(200).json({
-      message: "User Register Successfully",
-      success: true,
-      data: user,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "Server Error",
-      success: false,
-    });
-  }
-});
+router.post("/register", userController.register);
 
 module.exports = router;
