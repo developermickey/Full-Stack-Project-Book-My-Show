b489e9205e2f705ca63b4d926a95f8fcca80f430
developermickey <mukesh.quazma@gmail.com>
2025-10-29 22:50:35 +0530

    register route added

--- server/index.js
+++ server/index.js
@@ -3,9 +3,17 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 8001;
+
+const userRouter = require("./routes/user.route");
+
+app.use(express.json());
+app.use(express.urlencoded({ urlencoded: true }));
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+app.use("/api/auth", userRouter);
 
 connectDB();
 
---
+++ server/routes/user.route.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const User = require("../models/user.model");
+const router = express.Router();
+// signup, login, get user, forgot password
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(401).json({
+        message: "User already exists please login",
+        success: false,
+      });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    return res.status(200).json({
+      message: "User Register Successfully",
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      success: false,
+    });
+  }
+});
+
+module.exports = router;
