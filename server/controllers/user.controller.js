f5c7c14c09193a4bdad21ee0325727bafaa17b09
developermickey <mukesh.quazma@gmail.com>
2025-10-29 23:30:23 +0530

    Code added

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,20 +1,23 @@
 const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const userExist = await User.findOne({ email });
     if (userExist) {
-      return res.status(401).json({
+      return res.status(409).json({
         message: "User already exists please login",
         success: false,
       });
     }
 
+    const hashPassword = await bcrypt.hash(password, 10);
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashPassword,
     });
 
     return res.status(200).json({
